5bd1d81bfda779fb8718065a32f63b76957426fd
LacertaBasiliscusLinnaeus <bqt@tut.by>
2021-03-27 12:41:03 +0300

    initial commit

---
+++ src/labs/pm/app/Shop.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2021 VASILYK
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package labs.pm.app;
+
+import java.math.BigDecimal;
+import java.util.Locale;
+import labs.pm.data.ProductManager;
+import labs.pm.data.Rating;
+
+/**
+ * {@code Shop} class represents an application that manages Products
+ *
+ * @version 4.0
+ * @author VASILYK
+ */
+public class Shop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        var pm = new ProductManager(Locale.forLanguageTag("ru-RU"));
+        var p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
+        pm.printProductReport();
+        p1 = pm.reviewProduct(p1, Rating.FIVE_STAR, "The best tea ever!");
+        pm.printProductReport();
+
+//        var p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.THREE_STAR);
+//        var p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
+//        var p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
+//
+//        var p4 = pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99), Rating.TWO_STAR, LocalDate.now());
+//        var p5 = p3.applyRating(Rating.THREE_STAR);
+//
+//        var p6 = pm.createProduct(104, "Chokolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
+//        var p7 = pm.createProduct(104, "Chokolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
+//
+//        var p8 = p4.applyRating(Rating.FIVE_STAR);
+//        var p9 = p1.applyRating(Rating.TWO_STAR);
+//        if (p3 instanceof Food) {
+//            LocalDate bestBefore = ((Food) p3).getBestBefore();
+//        }
+//        System.out.println(p1.getBestBefore());
+//        System.out.println(p3.getBestBefore());
+//
+//        System.out.println(p6.equals(p7));
+//
+//        Product[] products = {p1, p2, p3, p4, p5, p6, p7, p8, p9};
+//
+//        for (var p : products) {
+//            System.out.println(p);
+//        }
+    }
+
+}
---
+++ src/labs/pm/data/Product.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2021 VASILYK
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package labs.pm.data;
+
+import java.math.BigDecimal;
+import static java.math.RoundingMode.HALF_UP;
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * {@code Product} class represents properties and behaviors of product objects
+ * in the Product Management System.
+ * <br>
+ * Each product has an id, name and price
+ * <br>
+ * Each product can have a discount, calculated based on a
+ * {@link DISCOUNT_RATE discount rate }
+ *
+ * @version 4.0
+ * @author VASILYK
+ */
+public abstract class Product implements Rateable<Product> {
+
+    private int id;
+    private String name;
+    private BigDecimal price;
+    private Rating rating;
+
+    /**
+     * A constant that defines a {@link java.math.BigDecimal BigDecimal} value
+     * of discount rate
+     * <br>
+     * Discount rate is 10%
+     */
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+
+    Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    Product(int id, String name, BigDecimal price) {
+        this(id, name, price, Rating.NOT_RATED);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    /**
+     * Calculates discount based on product price and
+     * {@link DISCOUNT_RATE discount rate }
+     *
+     * @return a {@link java.math.BigDecimal BigDecimal} value of the discount
+     */
+    public BigDecimal getDiscount() {
+        return this.getPrice().multiply(Product.DISCOUNT_RATE).setScale(2, HALF_UP);
+    }
+
+    @Override
+    public Rating getRating() {
+        return rating;
+    }
+
+    /**
+     * Get the value of the best before for the product
+     *
+     * @return the value of bestBefore
+     */
+    public LocalDate getBestBefore() {
+        return LocalDate.now();
+    }
+
+    @Override
+    public String toString() {
+        return id + ", " + name + ", " + price + ", " + getDiscount() + ", " + rating.getStars() + ", " + getBestBefore();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj instanceof Product) {
+            final Product other = (Product) obj;
+            return this.id == other.id && Objects.equals(this.name, other.name);
+        }
+
+        return false;
+    }
+
+}
